2071aab3f43930ce2ddb39d33b3bc4a7dcabfe46
shupopo-999 <shu999.20099@gmail.com>
2024-12-12 01:47:48 +0900

    ｳﾝﾁ

--- src/components/SNSPage.tsx
+++ src/components/SNSPage.tsx
@@ -9,17 +9,17 @@ const SNSPage = () => {
       <div className="sns-content">
         <div className="sns-card">
           <h2>X(旧Twitter)</h2>
-          <p>X(旧Twitter)の最新情報をお届けします。</p>
+          <p>X(旧Twitter)ではたまにツイートしたりします。</p>
           <a href="https://x.com/shu125481/">Twitterを見る</a>
         </div>
         <div className="sns-card">
           <h2>Youtube</h2>
-          <p>Youtubeの最新情報をお届けします。</p>
+          <p>Youtubeではたまに趣味のゲームを投稿したりします。</p>
           <a href="https://www.youtube.com/@shu_9999/">Youtubeを見る</a>
         </div>
         <div className="sns-card">
           <h2>Instagram</h2>
-          <p>Instagramの最新情報をお届けします。</p>
+          <p>Instagramはあまりやっておりません。(見る専です)</p>
           <a href="https://www.instagram.com/">Instagramを見る</a>
         </div>
       </div>
