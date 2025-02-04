f9f175420d9384392b534de91acbcd43b2d0b4c0
shupopo-999 <shu999.20099@gmail.com>
2025-02-04 15:22:20 +0900

    fix: 一部修正とブログの内容追加

--- src/components/BlogPage.tsx
+++ src/components/BlogPage.tsx
@@ -22,6 +22,20 @@ const BlogPage = () => {
                 </p>
               </h6>
             </div>
+            <div className="services text-padding-bottom centered-text">
+              <img src={MinecraftImage} />
+              <h3>現在やろうとしていること</h3>
+              <h6 className="left-aligned-text">
+                <p>
+                  現在の就職活動が一息ついたら、ToDoリストとスケジュール表見たいなのを作ろうとしていてその他にもアカウント認証<br/>
+                  とかもつけて第三者にもスケジュールやToDoリストを共有できるようなアプリケーションを作りたいなと考えております。<br/>
+                  例に上げると1つのサイトにGoogleカレンダーやスプレットシート、Googleドキュメントなどを詰め込んだようなアプリケー<br />
+                  ーションですね。<br />
+                  これが実現できればチーム制作がめっちゃ捗るし、連携も取りやすいと思うんですよね。個人的にはWebだけにしておくと<br />
+                  楽だけど、AppleStoreとかGoogleStoreにリリース出来れば通知が流せるのでさらに便利になりそうな感じが...　でも大変そうな作業が...
+                </p>
+              </h6>
+            </div>
           </div>
         </div>
       </section>
